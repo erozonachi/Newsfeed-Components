ea057e821d1ffd001e684d1db7ff8bc0f6cec49b
erozonachi <erozonachi.james@gmail.com>
2019-05-15 13:37:44 +0100

    Build the articles component logic
    Import `Article.less` from the correct path into index.less. Build
    out the logics to expand or hide an article's content.

--- Article/Article.js
+++ Article/Article.js
@@ -1,19 +1,27 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+const buttonLabelExpand = `expand`;
+const buttonLabelHide = `hide`;
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector(`.expandButton`);
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent = buttonLabelExpand;
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandButton.addEventListener(`click`, this.expandArticle);
     
   }
 
-  expandArticle() {
+  expandArticle = () => {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.classList.toggle(`article-open`);
+    this.expandButton.textContent === buttonLabelExpand?
+      this.expandButton.textContent = buttonLabelHide :
+      this.expandButton.textContent = buttonLabelExpand;
 
   }
 }
@@ -26,4 +34,8 @@ class Article {
 
 */
 
-let articles;
+const articles = document.querySelectorAll(`.article`);
+
+articles.forEach(article => {
+  new Article(article);
+});
